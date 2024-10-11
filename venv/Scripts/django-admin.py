8e7957bb50a2aa6b28f167511a1d9fe8607d1db3
xiaohai <927456729@qq.com>
2024-10-11 15:57:41 +0800

    Remove .idea and venv from tracking

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\xashuoxing\Desktop\小海文件\小程序开发\资料\Python_Django_Hospital_registration-master\PythonProject\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
